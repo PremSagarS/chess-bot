2b5dcf215a925c7c762ec70cea4ab8ff18d1d0f7
Prem Sagar S <reallypremsagar@gmail.com>
2023-09-24 21:52:17 +0530

    Move now stores details on promotion, castling

--- Move.py
+++ Move.py
@@ -2,28 +2,14 @@ from ChessConstants import *
 
 
 class Move:
-    def __init__(self, start_square, end_square, piece) -> None:
+    def __init__(
+        self, start_square, end_square, piece, promoteTo=None, castle=4
+    ) -> None:
         self.start_square = start_square
         self.end_square = end_square
         self.piece = piece
-
-    def get_start_square(self):
-        return self.start_square
-
-    def get_end_square(self):
-        return self.end_square
-
-    def get_piece(self):
-        return self.piece
-
-    def set_start_square(self, start_square):
-        self.start_square = start_square
-
-    def set_end_square(self, end_square):
-        self.end_square = end_square
-
-    def set_piece(self, piece):
-        self.piece = piece
+        self.promoteTo = promoteTo
+        self.castle = castle
 
     def __repr__(self) -> str:
         start_square = idx_to_square(self.start_square)

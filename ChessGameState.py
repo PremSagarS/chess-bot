e08c0b4d2f139f9cd1cf91fa5c2bc72ca0b37c79
Prem Sagar S <reallypremsagar@gmail.com>
2023-09-24 16:46:49 +0530

    Initialized GameState

--- ChessGameState.py
+++ ChessGameState.py
@@ -1,9 +1,20 @@
 class ChessGameState:
     def __init__(self):
-        self.chessboard = [0b1100,0b1010,0b1011,0b1101,0b1110,0b1011,0b1010,0b1100] + [0b1001] * 8 + [0b0000] * 32 + [0b0001] * 8 + [0b0100,0b0010,0b0011,0b0101,0b0110,0b0011,0b0010,0b0100]
-        self.move_timer = 0.0
-        self.fen_notation = ""
-        self.castling_rights = [False, False, False, False]
+        self.chessboard = (
+            [0b1100, 0b1010, 0b1011, 0b1101, 0b1110, 0b1011, 0b1010, 0b1100]
+            + [0b1001] * 8
+            + [0b0000] * 32
+            + [0b0001] * 8
+            + [0b0100, 0b0010, 0b0011, 0b0101, 0b0110, 0b0011, 0b0010, 0b0100]
+        )
+        self.move_timer = 0
+        self.move_counter = 1
+        self.fen_notations = []
+        self.castling_rights = [True] * 4
         self.en_passant_square = None
         self.moves = []
-        self.pieces = [None]*32
+        self.pieces = []
+        self.gen_pieces()
+
+    def gen_pieces(self):
+        pass
